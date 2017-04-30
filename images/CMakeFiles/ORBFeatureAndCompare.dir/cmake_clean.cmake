cb5b080411c9c492b9174152306f07ce62ae445c
lgybetter <437675103@qq.com>
2017-04-30 20:42:41 +0800

    finish the image detect and match using BOW

---
+++ images/CMakeFiles/ORBFeatureAndCompare.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ORBFeatureAndCompare.dir/ORBFeatureAndCompare.cpp.o"
+  "ORBFeatureAndCompare.pdb"
+  "ORBFeatureAndCompare"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ORBFeatureAndCompare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
